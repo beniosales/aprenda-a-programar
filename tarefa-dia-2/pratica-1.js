ad83e575a9581d6d2d2dcf2a81c711a212b1ddca
Benio <beniosales.jr@gmail.com>
2022-08-24 18:53:46 -0300

    tarefa 2 pratica 1.1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,2 +1,2 @@
-let nome="Benio", email, profissao, idade="27", lugar;
-console.log(nome, idade);
+let nome="Benio", email="beniosales.jr@gmail.com", profissao="estudante", idade="27", lugar="japão";
+console.log(nome, email, profissao, idade, lugar);
