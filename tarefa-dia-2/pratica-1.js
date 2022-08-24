92a3018d9597af4eb2496a394834fce58e21f708
Benio <beniosales.jr@gmail.com>
2022-08-24 19:02:43 -0300

    tarefa 2 pratica 1.3

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,3 +1,3 @@
 let nome="Benio", email="beniosales.jr@gmail.com", profissao="estudante", idade="27", lugar="Japão";
 //console.log(nome, email, profissao, idade, lugar);
-console.log("Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+console.log("Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + `e o seu email é ${email}` + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
